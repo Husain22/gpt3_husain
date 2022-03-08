8d7b56ec723a12f5d5a573a03a82f4e9acc692b0
knight107 <husain_107@outlook.com>
2022-03-09 00:01:46 +0300

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import {Cta, Brand, Navbar } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <Cta></Cta>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
 export default App;
